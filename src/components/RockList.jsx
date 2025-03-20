f4340956ea50f356bd6e4c96f2c996a6b5071b3f
Kai Swain <kaiferseain@icloud.com>
2025-03-19 23:15:12 -0500

    for my mac

--- src/components/RockList.jsx
+++ src/components/RockList.jsx
@@ -1,18 +1,25 @@
 import { useEffect } from "react"
 
-export const RockList = ({ rocks, fetchRocks }) => {
+export const RockList = ({ rocks, fetchRocks, setRocks }) => {
     useEffect(() => {
+        fetchRocks().then
     }, [])
 
     const displayRocks = () => {
         if (rocks && rocks.length) {
-            return rocks.map(rock => <div key={`key-${rock.id}`} className="border p-5 border-solid hover:bg-fuchsia-500 hover:text-violet-50 rounded-md border-violet-900 mt-5 bg-slate-50">
-                {rock.name} ({rock.type.label})
+            return rocks.map(rock => <div key={`key-${rock?.id}`} className="border p-5 border-solid hover:bg-fuchsia-500 hover:text-violet-50 rounded-md border-violet-900 mt-5 bg-slate-50">
+                {rock?.name} ({rock.type.label}) weighs {rock.weight} kg <br/>
+                In the collection of {rock.user?.first_name} {rock.user?.last_name}
             </div>)
         }
 
         return <h3>Loading Rocks...</h3>
     }
+
+    useEffect(() => {
+
+        displayRocks()
+    },[rocks])
 
     return (
         <>
